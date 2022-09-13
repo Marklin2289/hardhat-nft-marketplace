64c4b3bb6c359f96f419bff4f48cfdc74fceecdd
marklin2289 <marklin2289@gmail.com>
2022-09-12 20:41:04 -0700

    added listItem() with modifiers

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 error NftMarketplace__PriceMustBeAboveZero();
+error NftMarketplace__NotApprovedForMarketplace();
+error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
 
 contract NftMarketplace is ReentrancyGuard {
     struct Listing {
@@ -12,8 +14,28 @@ contract NftMarketplace is ReentrancyGuard {
         address seller;
     }
 
+    // events
+    event ItemListed(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    // NFT Contract address -> NFT TokenID -> Listing
     mapping(address => mapping(uint256 => Listing)) private s_listings;
+
     mapping(address => uint256) private s_proceeds;
+
+    // modifiers
+    modifier notListed(address nftAddress, uint256 tokenId, address owner){
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if(listing.price > 0) {
+            revert NftMarketplace__AlreadyListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
 
     // Main functions
     function listItem(address nftAddress, uint256 tokenId, uint256 price)external{
@@ -23,5 +45,10 @@ contract NftMarketplace is ReentrancyGuard {
         // 1. Send the NFT to the  contract, Transfer -> Contract "hold" the NFT. ❎  OR 
         // 2. Owners can still hold their NFT, and give the marketplace approval to sell the NFT for them. ✅
         IERC721 nft = IERC721(nftAddress);
+        if(nft.getApproved(tokenId) != address(this)) {
+            revert NftMarketplace__NotApprovedForMarketplace();
+        }
+        s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
     }
 }
